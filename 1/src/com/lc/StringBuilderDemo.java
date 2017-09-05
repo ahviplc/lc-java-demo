dceb0b45a461869c027cf9ce395c9fd4bbb4e670
ahviplc <ahlc@sina.cn>
2017-09-05 17:29:06 +0800

    StringBuilder demo append ！~2017-9-5 17:29:02

---
+++ 1/src/com/lc/StringBuilderDemo.java
@@ -0,0 +1,14 @@
+package com.lc;
+
+public class StringBuilderDemo {
+	
+	public static void main(String[] args) {
+		
+		StringBuilder sb1=new StringBuilder();
+		sb1.append(1);
+		sb1.append("LC");
+		System.out.println(sb1);
+			
+	}
+
+}
