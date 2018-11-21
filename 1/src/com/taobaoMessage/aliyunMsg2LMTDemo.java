a19f3cb5628b3c470115e8dd348c7ffe120d651c
ahviplc <ahlc@sina.cn>
2018-11-21 12:26:38 +0800

    1：进制转换工具类以及16进制值;2：String/Byte之间的转换等工具类1 2 3！ LC 2018年11月21日12:26:08

--- 1/src/com/taobaoMessage/aliyunMsg2LMTDemo.java
+++ 1/src/com/taobaoMessage/aliyunMsg2LMTDemo.java
@@ -26,8 +26,8 @@ public class aliyunMsg2LMTDemo {
 		final String product = "Dysmsapi";//短信API产品名称（短信产品名固定，无需修改）
 		final String domain = "dysmsapi.aliyuncs.com";//短信API产品域名（接口地址固定，无需修改）
 		//替换成你的AK
-		final String accessKeyId = "#";//你的accessKeyId,参考本文档步骤2
-		final String accessKeySecret = "#";//你的accessKeySecret，参考本文档步骤2
+		final String accessKeyId = "LTAIUwEYGHw49IRQ";//你的accessKeyId,参考本文档步骤2
+		final String accessKeySecret = "###";//你的accessKeySecret，参考本文档步骤2
 		//初始化ascClient,暂时不支持多region（请勿修改）
 		IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId,
 		accessKeySecret);
@@ -38,7 +38,7 @@ public class aliyunMsg2LMTDemo {
 		 //使用post提交
 		 request.setMethod(MethodType.POST);
 		 //必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式
-		 request.setPhoneNumbers("13818332325");//13916805133
+		 request.setPhoneNumbers("15855891980");//13916805133
 		 //必填:短信签名-可在短信控制台中找到
 		 request.setSignName("罗美特平台");
 		 //必填:短信模板-可在短信控制台中找到 -SMS_105980105      -SMS_142947036
