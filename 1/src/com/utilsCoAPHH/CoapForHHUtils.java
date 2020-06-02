e1d3aa39038b87cc1ff3eba45fd852d5cc64c100
ahviplc <ahlc@sina.cn>
2020-06-02 12:59:29 +0800

    完善了鸿鹄CoAP对接所用的相关校验工具类和格式化了代码,新增了测试和改善了方法! LC 2020-6-2 12:59:02

--- 1/src/com/utilsCoAPHH/CoapForHHUtils.java
+++ 1/src/com/utilsCoAPHH/CoapForHHUtils.java
@@ -5,16 +5,17 @@ public class CoapForHHUtils {
     public final static String primary_key = NumberUtils.byteArrayToHexString(new byte[]{0X01, 0X02, 0X03, 0X04, 0X05, 0X06, 0X07, 0X08, 0X09, 0X0A, 0X0b, 0X0c, 0x0d, 0x0e, 0x0f, 0x10});
 
     public static void main(String[] args) throws Exception {
+        String code = "eb55d721e30d4f59fbe5c791f39d5fe9";
         String did = "1";
         String body = "68000100ab0141300120052716515400040001103131323532303032303431393030303530303030303030303030303030303030000100000100100000eb55d721e30d4f59fbe5c791f39d5fe9ffae000d00924c9870000000053836383136333034343230303036384d3533313100000000004d353331312d4d434d4830533033000000000000011b44e372866714d0e831188d7dba68a59e76de47b0530fd70a5736a7946cef4c5f3516";
         String openBody = "680001003C0288000116097FB062DF511D0A4B56E227609874AD040F602EC70ECA9F5E5EDE4B620A3809A8203D1AFB07161454ACC12EBD789C6A8416";
         String data = body.substring(18, body.length() - 70);
         // 认证
-        System.out.println(verifyMac(did, body));
+        System.out.println(verifyMac(did, code, body));
         // 加密
-        System.out.println(getAesData(did, "00"));
+        System.out.println(getAesData(did, code, "00")); // 16097fb062df511d0a4b56e227609874
         // 解密
-        System.out.println(decryptData(did, "16097fb062df511d0a4b56e227609874"));
+        System.out.println(decryptData(did, code, "16097fb062df511d0a4b56e227609874"));
         // 消息序号开始到数据域(加密或认证)
         System.out.println(CRC_XModem("01413001" + "20052716515400040001103131323532303032303431393030303530303030303030303030303030303030000100000100100000eb55d721e30d4f59fbe5c791f39d5fe9ffae000d00924c9870000000053836383136333034343230303036384d3533313100000000004d353331312d4d434d483053303300000000000001" + "1b44e372866714d0e831188d7dba68a59e76de47b0530fd70a5736a7946cef4c"));
         //HexAscii转String
@@ -29,37 +30,38 @@ public class CoapForHHUtils {
         System.out.println(NumberUtils.byteArrayToHexString(bytesTemp)); // 0102030405060708090a0b0c0d0e0f10
         byte[] bytesTemp2 = new byte[]{0X01, 0X02, 0X03, 0X04, 0X05, 0X06, 0X07, 0X08, 0X09, 0X0A, 0X0b, 0X0c, 0x0d, 0x0e, 0x0f, 0x10};
         System.out.println(NumberUtils.byteArrayToHexString(bytesTemp2)); // 0102030405060708090a0b0c0d0e0f10
+        // 加密3003 20060114044400000000000100000000000016ce6100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000181c00200000138800000000000000000000000000000000000000000b0b0b0b0b0b0b0b0b0b0b
+        System.out.println(decryptData(did, "8f05ee23f6d1a0acbd0226178e6d8aa2", "c19ba2d55d4495ccc0c8863e3ea3d78bfd926f3a0c2fecefb6d9f2ac2288ec912d3803a5ee7df8eb2e008bdc905675942d3803a5ee7df8eb2e008bdc905675942d3803a5ee7df8eb2e008bdc905675942d3803a5ee7df8eb2e008bdc905675942d3803a5ee7df8eb2e008bdc9056759456a6f7a29105a0abf5cef5a5d8a0982fe929a0e08a578c4b4161e3a9e4c9ac5dfe28a1cbf09d14b25df25cae61be1dd4"));
+        // 加密3003 20060114510400000000000100000000000016cb6100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000181c00200000138800000000000000000000000000000000000000000b0b0b0b0b0b0b0b0b0b0b
+        System.out.println(decryptData(did, "690666a6cdd69bcf963ad96ac1f915f5", "5737efce287782e9216c1182abdc7e7dcbbc4e555ec367dc7d71eb4146964201cf72e0b4913eb9dd45957fca1dbe3f6fcf72e0b4913eb9dd45957fca1dbe3f6fcf72e0b4913eb9dd45957fca1dbe3f6fcf72e0b4913eb9dd45957fca1dbe3f6fcf72e0b4913eb9dd45957fca1dbe3f6feccf762d32c4424ae4acfa06b865ab817766e053bcb4c8c6bf768cf997f520249733b0ee0dfdd63bea8fe576e1db3258"));
     }
 
     //randCode 通讯随机码
     //primaryKey 定死的
     //getAesData 加密
     //getMac 认证
-    public static boolean verifyMac(String deviceId, String body) {
+    public static boolean verifyMac(String deviceId, String code, String body) {
         if (body.length() >= 272) {
             String data = body.substring(18, body.length() - 70);
             //20052716515400040001103131323532303032303431393030303530303030303030303030303030303030000100000100100000eb55d721e30d4f59fbe5c791f39d5fe9ffae000d00924c9870000000053836383136333034343230303036384d3533313100000000004d353331312d4d434d483053303300000000000001
-            String mac = getMac(deviceId, data);
+            String mac = getMac(deviceId, code, data);
             System.out.println(mac);
             return body.contains(mac);
         }
         return false;
     }
 
-    public static String getMac(String deviceId, String data) {
-        String code = "eb55d721e30d4f59fbe5c791f39d5fe9";
+    public static String getMac(String deviceId, String code, String data) {
         String key = getHashKey(code, primary_key);
         return HMACSHA256.sha256_HMAC(NumberUtils.hexItr2Arr(code + data), NumberUtils.hexItr2Arr(key));
     }
 
-    public static String getAesData(String deviceId, String data) {
-        String code = "eb55d721e30d4f59fbe5c791f39d5fe9";
+    public static String getAesData(String deviceId, String code, String data) {
         String key = getAesKey(code, primary_key);
         return NumberUtils.byteArrayToHexString(AesUtilForCoAPHH.aesPKCS7PaddingEncryptBytes(NumberUtils.hexItr2Arr(data), NumberUtils.hexItr2Arr(key)));
     }
 
-    public static String decryptData(String deviceId, String data) {
-        String code = "eb55d721e30d4f59fbe5c791f39d5fe9";
+    public static String decryptData(String deviceId, String code, String data) {
         String aesKey = getAesKey(code, primary_key);
         byte[] bytes = AesUtilForCoAPHH.aesPKCS7PaddingDecrypt(NumberUtils.hexItr2Arr(data), NumberUtils.hexItr2Arr(aesKey));
         return NumberUtils.byteArrayToHexString(bytes);
