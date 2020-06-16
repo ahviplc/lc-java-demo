20c0077a9613e722861da2417b9ae1154cb4bfcf
ahviplc <ahlc@sina.cn>
2020-06-16 16:00:18 +0800

    更新了LC自己工具包JustToolc的简单示例 ! LC 2020-6-16 16:00:12

--- 1/src/com/JustToolcDemo/myToolsTest.java
+++ 1/src/com/JustToolcDemo/myToolsTest.java
@@ -1,12 +1,48 @@
 package com.JustToolcDemo;
 
-import com.lc.utils.UDateUtils;
-import com.lc.utils.UStringUtils;
+import com.lc.core.StrFormatter;
+import com.lc.lang.UConsole;
+import com.lc.utils.UDateUtil;
+import com.lc.utils.UNumberUtil;
+import com.lc.utils.UStringUtil;
+
+import java.util.Date;
 
 public class myToolsTest {
     public static void main(String[] args) {
-        System.out.println(UStringUtils.toUpper("lc"));
-        System.out.println(UStringUtils.toLower("LC"));
-        System.out.println(UDateUtils.getTimeStamp());
+        System.out.println(UStringUtil.toUpper("lc"));
+        System.out.println(UStringUtil.toLower("LC"));
+
+        System.out.println(UDateUtil.getTimeStamp());
+        System.out.println(UDateUtil.getNowDate_EN());
+        System.out.println(UDateUtil.currentStr());
+        System.out.println(UDateUtil.getNowTime_EN());
+        System.out.println(UDateUtil.getNowTime_EN_yMdH());
+        System.out.println(UDateUtil.getNowTime_CN_yMdH());
+        System.out.println(UDateUtil.getNowTime_EN_yMdHm());
+        System.out.println(UDateUtil.getNowTime_EN_yyyyMMddHHmm());
+        System.out.println(UDateUtil.getNowTime_CN_yMdHm());
+        System.out.println(UDateUtil.getNowTime_CN_HHmmss());
+
+        UConsole.log(1);
+        UConsole.log("5213344");
+        UConsole.log(new Date());
+        UConsole.log(UDateUtil.currentStr());
+        UConsole.log("我是{},今年{},忍!", "LC", 18);
+        UConsole.print("print");
+        UConsole.print("我是{}", "print");
+        UConsole.log();
+        UConsole.print(StrFormatter.format("我是{},今年{},忍!", "LC2", 19));
+        UConsole.log();
+        UConsole.error("error");
+        UConsole.error("我是{}", "error");
+
+        UConsole.log(UNumberUtil.add(1, 1)); // 2.0
+        UConsole.log(UNumberUtil.add(2.22f, 1.11f)); // 3.33
+        UConsole.log(UNumberUtil.sub(522.3344, 1)); // 521.3344
+        UConsole.log(UNumberUtil.mul(100, 5.21)); // 521.0
+        UConsole.log(UNumberUtil.div(100, 3)); // 33.3333333333
+
+
     }
 }
