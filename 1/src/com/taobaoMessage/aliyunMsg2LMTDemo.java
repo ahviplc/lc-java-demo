9ffac22ecf0c955af58cd604f76f25f1b4424c24
ahviplc <ahlc@sina.cn>
2020-06-22 10:58:39 +0800

    去掉阿里云accessKeyId ! LC 2020-6-22 10:58:28

--- 1/src/com/taobaoMessage/aliyunMsg2LMTDemo.java
+++ 1/src/com/taobaoMessage/aliyunMsg2LMTDemo.java
@@ -26,7 +26,7 @@ public class aliyunMsg2LMTDemo {
 		final String product = "Dysmsapi";//短信API产品名称（短信产品名固定，无需修改）
 		final String domain = "dysmsapi.aliyuncs.com";//短信API产品域名（接口地址固定，无需修改）
 		//替换成你的AK
-		final String accessKeyId = "LTAIUwEYGHw49IRQ";//你的accessKeyId,参考本文档步骤2
+		final String accessKeyId = "###";//你的accessKeyId,参考本文档步骤2
 		final String accessKeySecret = "###";//你的accessKeySecret，参考本文档步骤2
 		//初始化ascClient,暂时不支持多region（请勿修改）
 		IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId,
